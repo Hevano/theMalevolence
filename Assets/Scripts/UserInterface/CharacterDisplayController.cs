6c87b6328e8a76cfe2a8080144d610ca602a575a
KevinAlexV <Kev.vilanova@gmail.com>
2021-11-10 14:55:10 -0800

    Revert "Revert "Adjustments to HUD""
    This reverts commit 624d06c13dbae71c1156b04ea6cf55fac78cf57d.

--- Assets/Scripts/UserInterface/CharacterDisplayController.cs
+++ Assets/Scripts/UserInterface/CharacterDisplayController.cs
@@ -51,6 +51,7 @@ public class CharacterDisplayController : MonoBehaviour, IPointerClickHandler {
 
             ChangeHealth(Character.Health);
             ChangeCorruption(Character.Corruption);
+            ChangeName(Character.data.name);
             ChangeProfile(Character.data.avatar);
             ChangeThumbtack(Character.data.thumbtack);
 
@@ -69,6 +70,10 @@ public class CharacterDisplayController : MonoBehaviour, IPointerClickHandler {
     public void ChangeThumbtack(Sprite newTack)
     {
         _thumbtack.sprite = newTack;
+    }
+    public void ChangeName(string name)
+    {
+        NameDisplay.text = name;
     }
     public void ChangeHealth(int currentHealth) {
         HealthDisplay.text = currentHealth + "/" + Character.data.health;
