7c120412a8695195e1b832166d310a6aa1472e1d
Cole Atkinson <catkinson1227@gmail.com>
2021-11-17 15:40:05 -0800

    The Headmaster boss fight

--- Assets/Scripts/Cards/Card Effects/Summon Effect.cs
+++ Assets/Scripts/Cards/Card Effects/Summon Effect.cs
@@ -8,10 +8,32 @@ public class SummonEffect : CardEffect {
     [SerializeField] private Enums.Character Boss;
 
     public override IEnumerator ApplyEffect () {
+        PuzzleboxCharacter puzzlebox;
+        HeadmasterCharacter headmaster;
         switch (Boss) {
             case Enums.Character.PuzzleBox:
-                PuzzleboxCharacter boss = (PuzzleboxCharacter)GameManager.manager.foes[0];
-                yield return boss.SpawnShard();
+                puzzlebox = (PuzzleboxCharacter)GameManager.manager.foes[0];
+                yield return puzzlebox.SpawnShard();
+                break;
+            case Enums.Character.Headmaster:
+                headmaster = (HeadmasterCharacter)GameManager.manager.foes[0];
+                if (Random.Range(1, 2) == 1) {
+                    for (int i = 0; i < 4; i++) {
+                        yield return headmaster.SpawnStudent();
+                    }
+                } else {
+                    for (int i = 0; i < 2; i++) {
+                        yield return headmaster.SpawnFaculty();
+                    }
+                }
+                break;
+            case Enums.Character.Student:
+                headmaster = (HeadmasterCharacter)GameManager.manager.foes[0];
+                yield return headmaster.SpawnStudent();
+                break;
+            case Enums.Character.Faculty:
+                headmaster = (HeadmasterCharacter)GameManager.manager.foes[0];
+                yield return headmaster.SpawnFaculty();
                 break;
         }
     }
